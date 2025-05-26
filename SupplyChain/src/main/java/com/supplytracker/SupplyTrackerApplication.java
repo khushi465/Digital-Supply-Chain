16a0ccbb75110c0a173f92923f3459e04f1a44ce
Khushi465 <gamedevak1213@gmail.com>
2025-05-26 14:35:50 +0530

    Initialized Project

---
+++ SupplyChain/src/main/java/com/supplytracker/SupplyTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.supplytracker.entity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplyTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplyTrackerApplication.class, args);
+	}
+
+}
